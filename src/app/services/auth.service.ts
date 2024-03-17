48a06f6d9b31f87756b3422937ef1726f7d6bb07
Martim Paraíba <martim.paraiba@faurecia-aptoide.com>
2024-03-17 00:09:32 +0000

    Login page done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ElasticService } from './db/elastic.service';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -8,28 +9,51 @@ import { ElasticService } from './db/elastic.service';
 export class AuthService {
     
     constructor(
-        private elasticService: ElasticService
+        private elasticService: ElasticService,
+        private router: Router,
     ) { }
     
     isLoggedIn() {
-        /* const token = localStorage.getItem("login_token");
+        const token = localStorage.getItem("login_token");
 
-        if (token !== undefined) {
+        if (token !== null) {
             return true;
-        } */
+        }
 
         /* TODO this.elasticService."get user associated with token" */   
         return false;
     }
 
-    tryLogin(email: string, password: string) {
-        // elastic search for user with this email and hashed password
-
+    tryLogin(email: string, password: string): boolean {
+        // TODO elastic search for user with this email and hashed password
+        if (false) {
+            return false
+        }
 
         // create a login token 
         const token = "TODO";
 
         // store login token 
         localStorage.setItem('login_token', token)
+
+        return true
+    }
+
+    /**
+     * End user session
+     */
+    logout() {
+        localStorage.removeItem('login_token');
+        this.router.navigate(['/login']);
+    }
+
+    /**
+     * 
+     * @returns token or false if no token is found
+     */
+    getLoginToken() {
+        const token = localStorage.getItem("login_token");
+
+        return token? token : false;
     }
 }
